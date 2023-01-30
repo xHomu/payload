09679f281569f054333470fe71d5fba2a9f93731
Jacob Fletcher <jacobsfletch@gmail.com>
2023-01-30 11:52:09 -0500

    feat: deletes old media upon reupload #1897

--- src/collections/operations/update.ts
+++ src/collections/operations/update.ts
@@ -1,10 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+
 import httpStatus from 'http-status';
 import { Config as GeneratedTypes } from 'payload/generated-types';
 import { Where, Document } from '../../types';
 import { Collection } from '../config/types';
 import sanitizeInternalFields from '../../utilities/sanitizeInternalFields';
 import executeAccess from '../../auth/executeAccess';
-import { NotFound, Forbidden, APIError, ValidationError } from '../../errors';
+import { NotFound, Forbidden, APIError, ValidationError, ErrorDeletingFile } from '../../errors';
 import { PayloadRequest } from '../../express/types';
 import { hasWhereAccessResult } from '../../auth/types';
 import { saveVersion } from '../../versions/saveVersion';
@@ -15,6 +18,8 @@ import { afterChange } from '../../fields/hooks/afterChange';
 import { afterRead } from '../../fields/hooks/afterRead';
 import { generateFileData } from '../../uploads/generateFileData';
 import { getLatestEntityVersion } from '../../versions/getLatestCollectionVersion';
+import fileExists from '../../uploads/fileExists';
+import { FileData } from '../../uploads/types';
 
 export type Arguments<T extends { [field: string | number | symbol]: unknown }> = {
   collection: Collection
@@ -149,6 +154,39 @@ async function update<TSlug extends keyof GeneratedTypes['collections']>(
   });
 
   data = newFileData;
+
+  // /////////////////////////////////////
+  // Delete any associated files
+  // /////////////////////////////////////
+
+  if (collectionConfig.upload) {
+    const { staticDir } = collectionConfig.upload;
+
+    const staticPath = path.resolve(config.paths.configDir, staticDir);
+
+    const fileToDelete = `${staticPath}/${doc.filename}`;
+
+    if (await fileExists(fileToDelete)) {
+      fs.unlink(fileToDelete, (err) => {
+        if (err) {
+          throw new ErrorDeletingFile(t);
+        }
+      });
+    }
+
+    if (doc.sizes) {
+      Object.values(doc.sizes).forEach(async (size: FileData) => {
+        const sizeToDelete = `${staticPath}/${size.filename}`;
+        if (await fileExists(sizeToDelete)) {
+          fs.unlink(sizeToDelete, (err) => {
+            if (err) {
+              throw new ErrorDeletingFile(t);
+            }
+          });
+        }
+      });
+    }
+  }
 
   // /////////////////////////////////////
   // beforeValidate - Fields
